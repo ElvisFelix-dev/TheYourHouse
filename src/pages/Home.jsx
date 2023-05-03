d570d7cf8bbfd80874f58f8373929bf03b4afd27
Elvis Felix <elvisjaspion@gmail.com>
2023-05-03 17:40:09 -0300

    v1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,9 @@ import { Helmet } from 'react-helmet-async'
 export default function Home() {
   return (
     <section className="relative bg-[url('./assets/imgBg.jpg')] bg-cover bg-center bg-no-repeat">
+      <Helmet>
+        <title>YourHouse</title>
+      </Helmet>
       <div className="absolute inset-0 bg-white/75 sm:bg-transparent sm:from-white/95 sm:to-white/25 ltr:sm:bg-gradient-to-r rtl:sm:bg-gradient-to-l"></div>
 
       <div className="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8">
@@ -23,19 +26,19 @@ export default function Home() {
           </p>
 
           <div className="mt-8 flex flex-wrap gap-4 text-center">
-            <a
-              href="#"
+            <Link
+              to="/sign-in"
               className="block w-full rounded bg-rose-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-rose-700 focus:outline-none focus:ring active:bg-rose-500 sm:w-auto"
             >
               Fazer Login
-            </a>
+            </Link>
 
-            <a
-              href="#"
+            <Link
+              to="/initial"
               className="block w-full rounded bg-white px-12 py-3 text-sm font-medium text-rose-600 shadow hover:text-rose-700 focus:outline-none focus:ring active:text-rose-500 sm:w-auto"
             >
-              Fazer Cadastro
-            </a>
+              Explorar
+            </Link>
           </div>
         </div>
       </div>
