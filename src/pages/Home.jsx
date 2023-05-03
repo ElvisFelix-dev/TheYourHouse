20aa7a3b18cdabaee3b524ebe0eb86d3a4e6271b
Elvis Felix <elvisjaspion@gmail.com>
2023-05-03 14:10:05 -0300

    styling page home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,29 @@
 import { Link } from 'react-router-dom'
+import '../styles/styles.css'
 
 export default function Home() {
   return (
     <div className="bg-[url('./assets/imgBg.jpg')] bg-cover h-screen">
-      <h1>Hello Wolrd</h1>
+      <div className="father">
+        <div className="children">
+          <h1>
+            Está procurando um local confortável para se reunir com seus amigos
+            e familiares? <br />
+            Ou está pensando em adquir um imóvel?
+          </h1>
+          <br />
+          <h3>Até mesmo para festas, com amigos e familiares.</h3>
+          <br />
+          <h3>Na YourHouse voce encontra tudo e mais um pouco. </h3>
+          <br />
+          <br />
+          <Link to="/sign-in">
+            <button className="btn">
+              Faça seu login, e explore as oportunidades
+            </button>
+          </Link>
+        </div>
+      </div>
     </div>
   )
 }
